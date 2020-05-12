12567a83f60cda60b99d266e73844d344997d02e
rahul2u <rsembedded@gmail.com>
2020-05-13 00:07:53 +0530

    add a loop_statements.py

---
+++ Hands-On-Natural-Language-Processing-NLP-using-Python/3-Section-3-Python-Crash-Course/loop_statements.py
@@ -0,0 +1,20 @@
+# break,continue,pass
+#
+# for i in range(1,10):
+#     if i > 5:
+#         break
+#     print(i)
+
+# print 123,8,9,10
+
+for i in range(1,11):
+    if i >= 4 and i <= 7:
+        continue
+    print(i)
+
+# Pass
+i = 1
+if (i==1):
+    pass
+else:
+    pass
