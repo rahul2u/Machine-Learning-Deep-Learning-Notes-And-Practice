edefc96c465abd03753118bcd6e441c27fbc37f9
rahul2u <rsembedded@gmail.com>
2020-05-12 23:39:02 +0530

    add a loop.py

---
+++ Hands-On-Natural-Language-Processing-NLP-using-Python/2-Section-2-Getting-the-required-softwares/loops.py
@@ -0,0 +1,17 @@
+# loops
+
+a = 1
+while a <=10:
+    print("hello World")
+    a += 1
+
+# for loop
+for i in range(10):
+    print(i)
+
+# print 12345  five times
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(j,end='')
+    print()
