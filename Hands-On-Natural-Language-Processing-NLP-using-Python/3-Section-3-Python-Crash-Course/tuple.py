21d3569ec3e16c81097467ab985e1fdad1eb89b3
rahul2u <rsembedded@gmail.com>
2020-05-13 14:03:53 +0530

    add a list.py

---
+++ Hands-On-Natural-Language-Processing-NLP-using-Python/3-Section-3-Python-Crash-Course/tuple.py
@@ -0,0 +1,13 @@
+# tuple  is imutable data type
+
+tuple1 =(1,2,3,4,'hello','world')
+for index in range(0,len(tuple1)):
+    print(tuple1[index],end =' ')
+
+
+for i in tuple1:
+    print(i)
+
+
+# delete is not possible or you can use silicing or convert to list
+
