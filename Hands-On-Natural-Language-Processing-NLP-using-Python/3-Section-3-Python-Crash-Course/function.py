2ed628579017927f5bb1c43d0f9c1ccdc8a1a71d
rahul2u <rsembedded@gmail.com>
2020-05-18 10:16:23 +0530

    add a  file function.py

---
+++ Hands-On-Natural-Language-Processing-NLP-using-Python/3-Section-3-Python-Crash-Course/function.py
@@ -0,0 +1,21 @@
+# functions are block of code
+
+
+def sum(num1,nub2):
+    return num1 + nub2
+
+
+print("Sum of Numbers sum:", (12, 13))
+
+# length of list
+
+
+def length(list):
+    count = 0
+    for i in list:
+        count+= 1
+    return count
+
+
+print("Length of list:", length([1,2,3,4,5]))
+
