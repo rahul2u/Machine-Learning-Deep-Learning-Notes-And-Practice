112f4735e8c04d83d9273e16e10504c947e3d9ac
rahul2u <rsembedded@gmail.com>
2020-05-18 11:29:02 +0530

    add a  file classes.py

---
+++ Hands-On-Natural-Language-Processing-NLP-using-Python/3-Section-3-Python-Crash-Course/classes.py
@@ -0,0 +1,33 @@
+# classes is tamlates for objects
+# humman is class and rahul is object and properties
+# constructore is speial method of class
+
+
+class Fish:
+    def swim(self):
+        print("Fish can swim")
+
+    def eat(self):
+        print("Fish can eat")
+
+
+fish = Fish()
+fish.swim()
+fish.eat()
+
+# override the constructor
+
+class Game:
+    def __init__(self,name):
+        self.name =name
+
+    def start(self):
+        print(self.name,"game is started")
+
+    def end(self):
+        print(self.name, "game is stoped")
+
+
+game = Game("Counter Strike")
+game.start()
+game.end()
