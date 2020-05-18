506f7b40ec03cc5fc56271ad398da22606595150
rahul2u <rsembedded@gmail.com>
2020-05-18 23:09:06 +0530

    add a  file regular_expression.py

--- Hands-On-Natural-Language-Processing-NLP-using-Python/4-Section-4-Regular-Expressions/regular_exp_1.py
+++ Hands-On-Natural-Language-Processing-NLP-using-Python/4-Section-4-Regular-Expressions/regular_exp_1.py
@@ -7,6 +7,24 @@ match = re.match(r"[a-zA-Z]+", sentence2)
 print(match)   # None return because match looks the First patterns not going to the whole sentence
 # it looks the first pattern in whole sentence .Its not going to to look in at whole sentence.
 
+# so this problem we use the search
+match = re.search(r"[a-zA-Z]+", sentence2)
+print(match)            # first match character  is was
+
+# .,*, + ,^,$
+
+# start with pattern
+match = re.match("^1990", sentence2)
+print(match)
+
+# end match
+match = re.search("born$",sentence2)
+print(match)
+
+if re.search("born$",sentence2):
+    print("Match")
+else:
+    print("Not Match")
 
 
 
