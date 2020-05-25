5a4f75ee04dc5b15b596eaeb7ff48b47414586a2
Rahul Singh <rsembedded@gmail.com>
2020-05-25 16:42:15 +0530

    add a  stemming and lemmatization.py

---
+++ Hands-On-Natural-Language-Processing-NLP-using-Python/6-Section-6-NLP-Core/stemming_and _lemmetization.py
@@ -0,0 +1,64 @@
+#  Stemming and Lemmatiztion
+# Why stemming and lemmatization - same word diffrent form have diffrent meaning for  computer so we convert root or same form of word.
+
+# stemming -Stemming is process of  reducing the infected words to their words stem,base or root form.
+# example -going  =go
+#         goes = go
+#         gone = go
+
+'''
+
+So we are not duplicating different words when we have the same word in different forms.
+We are not taking those words as different words rather than we are taking them as the same word.
+So this has problems.
+
+The problem is that intermediate words that are represented in case of stemming may not have any meaning
+
+whatsoever.
+So you know this word intelligent has no meaning.
+
+Similarly when you are stemming the word final finally and so on you would find out that you get a word
+
+called Feiner and it means nothing right.
+
+It just means nothing.
+Example-intelligen ,fina
+'''
+
+
+
+''' 
+Lemmatization -Same as steamming but intermediate representation/root form has a meaning
+Example - intelligence -intelligent
+          intelligent - intelligent 
+          intelligently
+
+'''
+
+'''
+Stemmming - words root or intermediate representing word may be meaning full or may be not 
+Lemmatization - words are  root or intermediate representing word  meaning full 100 % or dictionary words.
+
+When use lemmatization or when use steamming.
+'''
+
+'''
+##################################################################
+Lemmatization
+1-Word representation have meaning
+2-Takes more time than stemming
+3-Use lemmatization when meaning  of word is important for Analysis.
+example- question and  answer system
+
+Steaming --
+1-Word representation may not  have any  meaning
+2-Takes less time .
+3-Use stemming  when meaning  of word is not important for analysis.
+example- spam detection
+
+##################################################################
+
+
+'''
+
+
