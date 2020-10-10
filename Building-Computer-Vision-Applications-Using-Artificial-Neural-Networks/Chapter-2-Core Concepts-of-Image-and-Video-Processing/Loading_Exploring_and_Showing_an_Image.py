9fdc46fbf54e669b7d74484ddae42d38c2003d0d
Rahul Singh <rsembedded@gmail.com>
2020-10-10 16:00:34 +0530

    Loading_Exploring_and_Showing_an_Image.py

---
+++ Building-Computer-Vision-Applications-Using-Artificial-Neural-Networks/Chapter-2-Core Concepts-of-Image-and-Video-Processing/Loading_Exploring_and_Showing_an_Image.py
@@ -0,0 +1,27 @@
+# import library
+import cv2
+
+# image path
+image_path = "images/marsrover.png"
+
+# read image
+img = cv2.imread(image_path)
+
+# image is numpy array
+print(img)
+
+# image properties
+print("Dimensions of Image ", img.ndim)
+print("Image Shape", img.shape)
+height, width , channels = img.shape
+print(height, width, channels)
+print("Image Height", img.shape[0])
+print("Image Width", img.shape[1])
+print("Image Channels", img.shape[2])
+print("Size of the Image Array", img.size)
+print(cv2.countNonZero(img))
+cv2.imshow("My Image", img)
+cv2.waitKey()
+
+
+
