adf22f6932a932e8e084826fced0d28c18658c51
Rahul Singh <rsembedded@gmail.com>
2020-11-03 15:26:50 +0530

    decision_tree.py file added

--- A-Beginners-Guide-to-Machine-Learning-in-Python)/Section-1-Introduction/decision_tree.py
+++ A-Beginners-Guide-to-Machine-Learning-in-Python)/Section-1-Introduction/decision_tree.py
@@ -1,6 +1,6 @@
 # import Libraries
 import pandas as pd
-from sklearn import svm
+from sklearn import tree
 
 # load data and splitting
 data = pd.read_csv("spambase/spambase.data")
@@ -10,7 +10,7 @@ data = data.sample(frac=1)
 x = data.drop(['1'], axis=1)
 y = pd.DataFrame(data, columns=['1'])
 # prepare and fit the model
-model = svm.SVC()
+model = tree.DecisionTreeClassifier()
 model.fit(x, y)
 accuracy =model.score(x,y)
 print(accuracy)
--- A-Beginners-Guide-to-Machine-Learning-in-Python)/Section-1-Introduction/knn.py
+++ A-Beginners-Guide-to-Machine-Learning-in-Python)/Section-1-Introduction/knn.py
@@ -1,16 +1,17 @@
 # import Libraries
 import pandas as pd
-from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
 
 # load data and splitting
 data = pd.read_csv("spambase/spambase.data")
-data = data.sample(frac=1)
-
+data = data.sample(data, frac=1)
 
 x = data.drop(['1'], axis=1)
 y = pd.DataFrame(data, columns=['1'])
 # prepare and fit the model
-model = svm.SVC()
+model = KNeighborsClassifier()
 model.fit(x, y)
-accuracy =model.score(x,y)
-print(accuracy)
+prediction =model.score(x,y)
+print(prediction)
+
+#
